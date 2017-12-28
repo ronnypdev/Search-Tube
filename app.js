157f79ccc29c1cfeb6092e92505913e7a8139d03
Ronny Perez <ronnypdev@gmail.com>
2017-12-27 22:16:51 -0500

    apikEY & endpoint added

---
+++ app.js
@@ -0,0 +1,7 @@
+// Shorthand for $( document ).ready()
+$(function() {
+
+    var apikEY = 'AIzaSyBQumRj1GI29PvcblSgu_TxtOcxscyrIeo';
+    var apiURl = 'https://www.googleapis.com/youtube/v3/search';
+    console.log( "ready!" );
+});
