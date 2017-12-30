de0556cfe6ac4d883682f22af91d23d2cfa77b5c
Ronny Perez <ronnypdev@gmail.com>
2017-12-30 00:49:04 -0500

    api connection stablished thumbnails missing

--- app.js
+++ app.js
@@ -3,5 +3,36 @@ $(function() {
 
     var apikEY = 'AIzaSyBQumRj1GI29PvcblSgu_TxtOcxscyrIeo';
     var apiURl = 'https://www.googleapis.com/youtube/v3/search';
-    console.log( "ready!" );
+
+    $('form').on('submit', function(evt) {
+        evt.preventDefault();
+        var searchTerm = $('#searchForm').val();
+
+        //object data sent for the server
+        youtubeData = {
+
+          part: 'snippet',
+          key: apikEY,
+          q: searchTerm
+
+        };
+
+        var youtubeResults = function(data) {
+
+          var videoList = '<ul>';
+
+          $.each(data.items, function(i, results) {
+             videoList += '<li>';
+             videoList += '<h3>' + results.snippet.title + '</h3>';
+             videoList += '<img src="' + results.snippet.thumbnails + '"></li>';
+          });
+
+          videoList+= '</ul>';
+          $('#search-out-put').html(videoList);
+
+        };
+
+
+        $.getJSON(apiURl, youtubeData, youtubeResults);
+    });
 });
