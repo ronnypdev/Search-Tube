1b17f1d9c8765b88f6d4323dc9bb6a73b90fc548
Ronny Perez <ronnypdev@gmail.com>
2018-03-25 23:17:31 -0400

    changes made

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
 // Shorthand for $( document ).ready()
 $(function() {
+  var apikEY = 'AIzaSyBQumRj1GI29PvcblSgu_TxtOcxscyrIeo';
+  var apiURl = 'https://www.googleapis.com/youtube/v3/search';
 
-    var apikEY = 'AIzaSyBQumRj1GI29PvcblSgu_TxtOcxscyrIeo';
-    var apiURl = 'https://www.googleapis.com/youtube/v3/search';
+  $('form').on('submit', function(evt) {
+    evt.preventDefault();
+    var searchTerm = $('#searchForm').val();
 
-    $('form').on('submit', function(evt) {
-        evt.preventDefault();
-        var searchTerm = $('#searchForm').val();
+    //object data sent for the server
+    youtubedata = {
+      part: 'snippet',
+      key: apikEY,
+      q: searchTerm,
+      maxResults: 25
+    };
 
-        //object data sent for the server
-        youtubedata = {
+    var youtubeResults = function(data) {
+      var videoList = '<ul>';
 
-          part: 'snippet',
-          key: apikEY,
-          q: searchTerm,
-          maxResults: 25,
-          order: viewCount
+      $.each(data.items, function(i, results) {
+        videoList += '<li>';
+        videoList += '<h3>' + results.snippet.title + '</h3>';
+        videoList +=
+          '<iframe src="' +
+          results.snippet.thumbnails.standard +
+          '"></iframe></li>';
+      });
 
-        };
+      videoList += '</ul>';
+      $('#search-out-put').html(videoList);
+      console.log(results.snippet.thumbnails.standard);
+    };
 
-        var youtubeResults = function(data) {
-
-          var videoList = '<ul>';
-
-          $.each(data.items, function(i, results) {
-             videoList += '<li>';
-             videoList += '<h3>' + results.snippet.title + '</h3>';
-             videoList += '<iframe src="'+ results.snippet.thumbnails.standard  + '"></iframe></li>';
-          });
-
-          videoList+= '</ul>';
-          $('#search-out-put').html(videoList);
-          console.log(youtubedata.key);
-
-        };
-
-
-        $.getJSON(apiURl, youtubedata, youtubeResults);
-    });
+    $.getJSON(apiURl, youtubedata, youtubeResults);
+  });
 });
